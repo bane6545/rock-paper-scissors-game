38a58dea504a4393d32032ef3484d54f0691771a
Aman Singh <amansingh464622@gmail.com>
2025-06-19 22:48:08 +0530

    Update rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -76,7 +76,7 @@ function pickComputerMove() {
   } else if (randonNumber >= 1 / 3 && randonNumber < 2 / 3) {
     computerMove = "paper";
   } else if (randonNumber >= 2 / 3 && randonNumber < 1) {
-    computerMove = "Scissors";
+    computerMove = "scissors";
   }
   return computerMove;
 }
